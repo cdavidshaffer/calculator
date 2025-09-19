57fab9c03db0e1943d8c8cb87cf68c3754dae809
C. David Shaffer <cdshaffer@acm.org>
2025-09-19 17:29:25 -0400

    Fixed bug 200

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 def main():
     print("Hello from calculator main, second version with bug 192 fixed, I think!")
     print("Oops, really fixed now")
+    print("Also fixed bug 200")
 
 
 if __name__ == "__main__":
