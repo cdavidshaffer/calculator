2e86c018e6e5a21f51ddeffe15bae50b68ac89db
C. David Shaffer <cdshaffer@acm.org>
2025-09-19 16:51:29 -0400

    Fixed bug 192

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from calculator main, second version!")
+    print("Hello from calculator main, second version with bug 192 fixed, I think!")
 
 
 if __name__ == "__main__":
